e5133ad3fd1555253fb00fa7daa2fd1faa363581
xansec <abrewer@forallsecure.com>
2025-01-30 02:19:31 -0500

    separate entire_file functionality

--- harness/fuzzing_utils.cc
+++ harness/fuzzing_utils.cc
@@ -1,81 +1,9 @@
+#include <vector>
 #include "fuzzing_utils.h"
 
 // Global variables
 FuzzedDataProvider* global_provider = nullptr;
 std::vector<FuzzTestFunc> fuzz_tests;
-
-/*
-     Reads an entire file into memory.
-     Example usage:
-
-     EntireFile file = read_entire_file_into_memory("some/path/to/file");
-     // We can also check file.len
-     if (!file.contents) {
-         // Could not read the file. Handle error
-     }
-
-     // File was read successfully
- */
-EntireFile read_entire_file_into_memory(const char* path) {
-    EntireFile res = {0};
-        long int file_size;
-        size_t bytes_read;
-
-    if (!path) {
-        fprintf(stderr, "No path was given to read\n");
-        return res;
-    }
-
-    FILE* file = fopen(path, "rb");
-    if (!file) {
-        fprintf(stderr, "Could not open %s for reading\n", path);
-        goto error;
-    }
-
-    if (fseek(file, 0, SEEK_END) != 0) {
-        fprintf(stderr, "Could not seek to the end of file %s\n", path);
-        goto error;
-    }
-
-    file_size = ftell(file);
-    if (file_size == -1L) {
-        fprintf(stderr, "Could not tell the size of file %s\n", path);
-        goto error;
-    }
-
-    if (fseek(file, 0, SEEK_SET) != 0) {
-        fprintf(stderr, "Could not set the cursor to the start of file %s\n", path);
-        goto error;
-    }
-
-    res.contents = MALLOC(file_size, char);
-    if (!res.contents) {
-        fprintf(stderr, "Could not allocate memory for storing contents of file %s\n", path);
-        goto error;
-    }
-
-    bytes_read = fread(res.contents, sizeof(char), file_size, file);
-    if (bytes_read != file_size) {
-        fprintf(stderr, "Could not read entire file %s\n", path);
-        goto error;
-    }
-    res.len = bytes_read;
-
-    fclose(file);
-
-    return res;
-
-error:
-    if (file) {
-        fclose(file);
-    }
-
-    if (res.contents) {
-        free(res.contents);
-    }
-
-    return (EntireFile) {0};
-}
 
 void RegisterFuzzTest(FuzzTestFunc func) {
     fuzz_tests.push_back(func);
--- harness/fuzzing_utils.h
+++ harness/fuzzing_utils.h
@@ -1,16 +1,8 @@
 #ifndef FUZZING_UTILS_H
 #define FUZZING_UTILS_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "entire_file_utils.h"
 #include <fuzzer/FuzzedDataProvider.h>
-
-#ifdef _MSC_VER
-// Disable warnings for using fopen when compiling with MSVC
-#define _CRT_SECURE_NO_WARNINGS
-#endif
-
-#define MALLOC(num_elements, type) ((type*) malloc((num_elements) * sizeof(type)))
 
 extern FuzzedDataProvider* global_provider;
 
@@ -30,14 +22,6 @@ extern FuzzedDataProvider* global_provider;
     void test_group##_##test_name##_FuzzTest()
 
 typedef void (*FuzzTestFunc)();
-
-// Structure representing the contents of a file in memory
-typedef struct EntireFile {
-    char* contents;
-    size_t len;
-} EntireFile;
-
-EntireFile read_entire_file_into_memory(const char* path);
 void RegisterFuzzTest(FuzzTestFunc func);
 void RunFuzzTests(const char* file_path);
 
